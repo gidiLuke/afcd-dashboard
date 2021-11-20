cf695829768dba0da498a6efcbfc0ef8e100280b
Lukas Heidegger <lukas.heidegger@tum.de>
2021-11-20 18:48:25 +0100

    Cleanup Dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,30 +1,14 @@
 <template>
+
   <div>
-    <WidgetsStatsA />
     <CRow>
-      <CCol :md="12">
+      <CCol :md="6">
         <CCard class="mb-4">
           <CCardBody>
             <CRow>
-              <CCol :sm="5">
-                <h4 id="traffic" class="card-title mb-0">Traffic</h4>
-                <div class="small text-medium-emphasis">January 2021</div>
-              </CCol>
-              <CCol :sm="7" class="d-none d-md-block">
-                <CButton color="primary" class="float-end">
-                  <CIcon icon="cil-cloud-download" />
-                </CButton>
-                <CButtonGroup
-                  class="float-end me-3"
-                  role="group"
-                  aria-label="Basic outlined example"
-                >
-                  <CButton color="secondary" variant="outline">Day</CButton>
-                  <CButton color="secondary" variant="outline" active
-                    >Month</CButton
-                  >
-                  <CButton color="secondary" variant="outline">Year</CButton>
-                </CButtonGroup>
+              <CCol :sm="12">
+                <h4 id="overall" class="card-title mb-0">Overall Statistics</h4>
+                <div class="small text-medium-emphasis">for the batch of images uploaded.</div>
               </CCol>
             </CRow>
             <CRow>
@@ -33,67 +17,33 @@
               />
             </CRow>
           </CCardBody>
-          <CCardFooter>
-            <CRow :xs="{ cols: 1 }" :md="{ cols: 5 }" class="text-center">
-              <CCol class="mb-sm-2 mb-0">
-                <div class="text-medium-emphasis">Visits</div>
-                <strong>29.703 Users (40%)</strong>
-                <CProgress
-                  class="mt-2"
-                  color="success"
-                  thin
-                  :precision="1"
-                  :value="40"
-                />
-              </CCol>
-              <CCol class="mb-sm-2 mb-0 d-md-down-none">
-                <div class="text-medium-emphasis">Unique</div>
-                <strong>24.093 Users (20%)</strong>
-                <CProgress
-                  class="mt-2"
-                  color="info"
-                  thin
-                  :precision="1"
-                  :value="20"
-                />
-              </CCol>
-              <CCol class="mb-sm-2 mb-0">
-                <div class="text-medium-emphasis">Pageviews</div>
-                <strong>78.706 Views (60%)</strong>
-                <CProgress
-                  class="mt-2"
-                  color="warning"
-                  thin
-                  :precision="1"
-                  :value="60"
-                />
-              </CCol>
-              <CCol class="mb-sm-2 mb-0">
-                <div class="text-medium-emphasis">New Users</div>
-                <strong>22.123 Users (80%)</strong>
-                <CProgress
-                  class="mt-2"
-                  color="danger"
-                  thin
-                  :precision="1"
-                  :value="80"
-                />
-              </CCol>
-              <CCol class="mb-sm-2 mb-0 d-md-down-none">
-                <div class="text-medium-emphasis">Bounce Rate</div>
-                <strong>Average Rate (40.15%)</strong>
-                <CProgress class="mt-2" :value="40" thin :precision="1" />
+        </CCard>
+      </CCol>
+      <CCol :md="6">
+        <CCard class="mb-4">
+          <CCardBody>
+            <CRow>
+              <CCol :sm="12">
+                <h4 id="distribution" class="card-title mb-0">Distribution of Faulty Chips</h4>
+                <div class="small text-medium-emphasis">This graph shows when the faulty chips were detected.
+                  The time is derived from the file name.</div>
               </CCol>
             </CRow>
-          </CCardFooter>
+            <CRow>
+              <MainChartExample
+                style="height: 300px; max-height: 300px; margin-top: 40px"
+              />
+            </CRow>
+          </CCardBody>
         </CCard>
       </CCol>
     </CRow>
-    <WidgetsStatsD />
+    <!--<WidgetsStatsD />-->
+
     <CRow>
       <CCol :md="12">
         <CCard class="mb-4">
-          <CCardHeader> Traffic &amp; Sales </CCardHeader>
+          <CCardHeader>Image Gallery</CCardHeader>
           <CCardBody>
             <CRow>
               <CCol :sm="12" :lg="6">
@@ -296,15 +246,11 @@ import avatar4 from '@/assets/images/avatars/4.jpg'
 import avatar5 from '@/assets/images/avatars/5.jpg'
 import avatar6 from '@/assets/images/avatars/6.jpg'
 import MainChartExample from './charts/MainChartExample'
-import WidgetsStatsA from './widgets/WidgetsStatsTypeA.vue'
-import WidgetsStatsD from './widgets/WidgetsStatsTypeD.vue'
 
 export default {
   name: 'Dashboard',
   components: {
-    MainChartExample,
-    WidgetsStatsA,
-    WidgetsStatsD,
+    MainChartExample
   },
   setup() {
     const progressGroupExample1 = [
