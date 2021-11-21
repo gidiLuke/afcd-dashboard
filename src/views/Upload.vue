68e3c3f758d8d9abae3d18212bc05e8e8042d4f4
Wenjie Hou <wenjie.hou@tum.de>
2021-11-21 09:38:53 +0100

    add upload logics

--- src/views/Upload.vue
+++ src/views/Upload.vue
@@ -11,66 +11,94 @@
                   <CCardBody>
                     <CRow>
                       <CCol :sm="12">
-                        <h4 id="upload-your-own" class="card-title mb-0">Upload your own images</h4>
-                        <div class="small text-medium-emphasis">Accepted: .PNG</div>
+                        <h4 id="upload-your-own" class="card-title mb-0">
+                          Upload your own images
+                        </h4>
+                        <div class="small text-medium-emphasis">
+                          Accepted: .PNG
+                        </div>
+                        <button
+                          v-if="!showSelect"
+                          class="btn btn-primary btn-block mid"
+                          @click="uploadImages"
+                        >
+                          Upload
+                        </button>
                       </CCol>
                     </CRow>
                     <CRow style="margin-top: 15px">
-                      <CCol :sm="12" >
+                      <CCol :sm="12">
                         <form id="file-upload-form" class="uploader">
-                          <input id="file-upload" type="file" name="fileUpload" accept="image/*"/>
+                          <input
+                            id="file-upload"
+                            ref="fileInput"
+                            type="file"
+                            name="fileUpload"
+                            accept="image/*"
+                            multiple
+                            @change="updateShowSelect"
+                          />
 
-                          <label for="file-upload" id="file-drag">
-                            <img id="file-image" src="#" alt="Preview" class="hidden">
+                          <label
+                            v-if="showSelect"
+                            id="file-drag"
+                            for="file-upload"
+                          >
+                            <img
+                              id="file-image"
+                              src="#"
+                              alt="Preview"
+                              class="hidden"
+                            />
                             <div id="start">
                               <i class="fa fa-download" aria-hidden="true"></i>
                               <div>Select a file or drag here</div>
-                              <div id="notimage" class="hidden">Please select an image</div>
-                              <span id="file-upload-btn" class="btn btn-primary">Select a file</span>
-                            </div>
-                            <div id="response" class="hidden">
-                              <div id="messages"></div>
-                              <progress class="progress" id="file-progress" value="0">
-                                <span>0</span>%
-                              </progress>
+                              <div id="notimage" class="hidden">
+                                Please select images
+                              </div>
+                              <span id="file-upload-btn" class="btn btn-primary"
+                                >Select a file</span
+                              >
                             </div>
                           </label>
                         </form>
-
-
                       </CCol>
-
                     </CRow>
                   </CCardBody>
                 </CCard>
-
               </CCol>
               <CCol :md="6">
-                <CCard class="mb-4" style="min-height: 335px;">
+                <CCard class="mb-4" style="min-height: 335px">
                   <CCardBody>
                     <CRow>
                       <CCol :sm="12">
-                        <h4 id="use-test-set" class="card-title mb-0">Use the provided test set</h4>
+                        <h4 id="use-test-set" class="card-title mb-0">
+                          Use the provided test set
+                        </h4>
                         <div class="small text-medium-emphasis">
-                          Click here to use the test set provided for hackatum 2021.</div>
+                          Click here to use the test set provided for hackatum
+                          2021.
+                        </div>
                       </CCol>
                     </CRow>
-                    <CRow style="margin-top: 100px;">
+                    <CRow style="margin-top: 100px">
                       <CCol :sm="12">
                         <form id="test-set" class="uploader test-set-button">
                           <div class="text-center">
-                              <a href="/#/dashboard" id="use-test-set-btn" class="btn btn-primary">Use Test Set</a>
+                            <a
+                              id="use-test-set-btn"
+                              href="/#/dashboard"
+                              class="btn btn-primary"
+                              >Use Test Set</a
+                            >
                           </div>
                         </form>
-
                       </CCol>
-
                     </CRow>
                   </CCardBody>
                 </CCard>
               </CCol>
             </CRow>
-
           </CCardBody>
         </CCard>
       </CCol>
@@ -87,33 +115,45 @@ export default {
   components: {},
   setup() {
     const fileInput = ref(null)
-    const imageList = []
     const processing = ref(false)
+    const showSelect = ref(true)
 
+    // prettier-ignore
     const { proxy } = getCurrentInstance()
-
+    // prettier-ignore
     const store = useStore()
 
     onMounted(() => {
       processing.value = false
     })
 
-    const uploadImages = () => {
-      processing.value = true
-      for (const file of fileInput.value.files) {
-        // convert file to base64 string
+    const updateShowSelect = () => {
+      showSelect.value =
+        fileInput.value == null ||
+        fileInput.value.files == null ||
+        fileInput.value.files.length === 0
+    }
+
+    function getBase64(file) {
+      return new Promise((resolve, reject) => {
         const reader = new FileReader()
         reader.readAsDataURL(file)
-        reader.onload = () => {
-          imageList.push(reader.result)
-        }
-      }
+        reader.onload = () => resolve(reader.result)
+        reader.onerror = (error) => reject(error)
+      })
+    }
 
-      proxy.$axios
+    const uploadImages = async () => {
+      processing.value = true
+      const imageList = []
+      Promise.all(Array.from(fileInput.value.files).map((file) => getBase64(file)))
+        .then((results) => {
+          return proxy.$axios
         .post(
-          'https://hackatumimageupload.azurewebsites.net/api/uploadImage?code=jskoDScmdLRL8xy7W7FQdO52qm3bDMHyRJj6lXvH7sYKYi1rza7xlw==',
-          { images: imageList },
+          'https://hasjdoi.azurewebsites.net/api/httptrigger?code=2lD/ki4dO30Y9nJcYZym28IATG1Xa8Jcb7fWlpPqaqutqFL9vRYNDg==',
+          { images: results },
         )
+        })
         .then((res) => {
           store.commit('update', res.data)
           processing.value = false
@@ -125,10 +165,17 @@ export default {
       uploadImages,
       fileInput,
       processing,
+      showSelect,
+      updateShowSelect,
     }
   },
   computed: {},
 }
 </script>
 
-<style></style>
+<style>
+.mid {
+  display: block !important;
+  margin: 0 auto !important;
+}
+</style>
